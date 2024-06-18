1136bc8ac48a8c7963e965242f19a474c0f3f923
melcholign <115886450+melcholign@users.noreply.github.com>
2024-06-18 17:41:35 +0600

    Create a game controller factory

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 /**
- * Creates a n x n tic-tac-toe gameboard
+ * Creates a n x n tic-tac-toe gameBoard
  * 
- * @param {int} n - width and height of the gameboard
- * @returns {Object} - gameboard object
+ * @param {int} n - width and height of the gameBoard
+ * @returns {GameBoard} - GameBoard object
  */
-function createGameboard(n) {
+function createGameBoard(n) {
 
     const board = [];
 
@@ -57,6 +57,152 @@ function createGameboard(n) {
      */
     const getMarkFrom = (row, col) => board[row, col];
 
-    return { setMarkAt, getMarkFrom, display};
+    return { setMarkAt, getMarkFrom, display };
 };
 
+const GAME_STATES = {
+    WIN: 0,
+    DRAW: 1,
+    ONGOING: 2,
+}
+
+/**
+ * Creates the game controller for tic-tac-toe on a n x n gameboard
+ * 
+ * @param {int} n - size of the game board 
+ * @returns {GameController} - game controller
+ */
+function createGameController(n) {
+
+    const gameBoard = createGameBoard(n);
+
+    /**
+     * Represents the state of the game
+     */
+    const gameState = { state: GAME_STATES.ONGOING, winSet: null, };
+
+    let playerOneActive = false;
+    let round = 0;
+
+    // sums of player marks that determine whether a row, column, or any of the diagonals
+    // are marked solely by a single player, which can be used to find the winner.
+    let rowSums = Array(n).fill(0);
+    let colSums = Array(n).fill(0);
+    let mainDiagonalSum = 0;
+    let crossDiagonalSum = 0;
+
+    /**
+     * Plays a round of the game
+     * 
+     * @param {int} row - row index of board
+     * @param {int} col - column index of board
+     * @returns {GameState} - the current state of the game
+     */
+    const playRound = (row, col) => {
+        // if game has already ended in a win or a draw, return the final game state
+        if (gameState.state === GAME_STATES.WIN || gameState.state === GAME_STATES.DRAW) {
+            return gameState;
+        }
+
+        // switch between players
+        playerOneActive = !playerOneActive;
+        round++;
+
+        try {
+
+            gameBoard.setMarkAt(row, col, playerOneActive);
+
+        } catch (error) {
+
+            console.error(error);
+
+            playerOneActive = !playerOneActive;
+
+            return gameState;
+        }
+
+        incrementSums(row, col);
+
+        if (round === n * n) {
+            gameState.state = GAME_STATES.DRAW;
+        }
+
+        if (round >= n * 2 - 1) {
+            const winSet = getWinSet(row, col);
+
+            if (winSet) {
+                gameState.state = GAME_STATES.WIN;
+                gameState.winSet = winSet;
+            }
+        }
+
+        gameBoard.display();
+        console.log({ rowSum: rowSums[row], colSum: colSums[col], mainDiagonalSum, crossDiagonalSum });
+
+        return gameState;
+    }
+
+    /**
+     * Increments the internal sum variables based on the 
+     * position of the new mark and the active player
+     * 
+     * @param {int} row 
+     * @param {int} col 
+     */
+    const incrementSums = (row, col) => {
+
+        const increment = playerOneActive ? 1 : -1;
+
+        rowSums[row] += increment;
+        colSums[col] += increment;
+        mainDiagonalSum += (row === col) ? increment : 0;
+        crossDiagonalSum += (row === n - col - 1) ? increment : 0;
+
+    }
+
+    /**
+     * Determines if a player has won by returning set of (row, column) indices pairs 
+     * that represents a linear section of the board. 
+     * 
+     * @param {int} row 
+     * @param {int} col 
+     * @returns {Array} - If no player has won, null is returned.
+     */
+    const getWinSet = (row, col) => {
+
+        const winSum = playerOneActive ? n : -n;
+        const winSet = [];
+
+        if (rowSums[row] === winSum) {
+            for (let i = 0; i < n; i++) {
+                winSet.push([row, i]);
+            }
+        }
+
+        else if (colSums[col] === winSum) {
+            for (let i = 0; i < n; i++) {
+                winSet.push([i, col]);
+            }
+        }
+
+        else if (row === col && mainDiagonalSum === winSum) {
+            for (let i = 0; i < n; i++) {
+                winSet.push([i, i]);
+            }
+        }
+
+        else if (row === n - col - 1 && crossDiagonalSum === winSum) {
+            for (let i = 0; i < n; i++) {
+                winSet.push([i, n - i - 1]);
+            }
+        }
+
+        else {
+            return null;
+        }
+
+        return winSet;
+    }
+
+    return { playRound };
+}
